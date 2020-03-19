6129ced1bab70d88cce314f5677ee1a206028531
Andrew1105 <34203705+Andrew1105@users.noreply.github.com>
2020-03-19 13:53:21 -0700

    Add files via upload

---
+++ gmodadminsuite-jobwhitelist-1.2.6/lua/gmodadminsuite/modules/jobwhitelist/_gas_info.lua
@@ -0,0 +1,10 @@
+return {
+	DefaultEnabled = true,
+	Name = "Billy's Whitelist",
+	Category = GAS.MODULE_CATEGORY_PLAYER_MANAGEMENT,
+	Wiki = "https://gmodsto.re/bwhitelist-wiki",
+	Icon = "icon16/vcard_edit.png",
+	DarkRP = true,
+	GmodStore = "6017",
+	License = '{"licensee":"76561198199946973","keys":{"xeon-de":"LABA622BF5A75C9DD732E786C4D80D552","xeon-us":"L27D4682AF7D3C0A464D7E7F06B4AB21C"}}',
+}
